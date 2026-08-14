2a0d564a6f141c301a1cb37f930ebee25a32fff4
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2026-08-14 13:15:11 +0100

    chore: remove UI specific request helpers

--- src/test/scala/uk/gov/hmrc/perftests/BaseRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/BaseRequests.scala
@@ -16,13 +16,7 @@
 
 package uk.gov.hmrc.perftests
 
-import io.gatling.core.Predef._
-import io.gatling.core.check.CheckBuilder
-import io.gatling.core.check.regex.RegexCheckType
-import io.gatling.http.Predef._
-import io.gatling.http.check.header.HttpHeaderRegexCheckType
 import uk.gov.hmrc.performance.conf.ServicesConfiguration
-import uk.gov.hmrc.perftests.AuthLogin.AuthLoginRequests.baseUrlFor
 
 trait BaseRequests extends ServicesConfiguration {
 
@@ -30,20 +24,5 @@ trait BaseRequests extends ServicesConfiguration {
   val authLoginApiUrl: String = s"$authBaseUrl/government-gateway/session/login"
   val formphost: String       = baseUrlFor("formp-proxy")
   val formpUrl                = s"$formphost/formp-proxy"
-
-  val authLoginStub: String = baseUrlFor("auth-login-stub")
-  val authLoginStubUrl      = s"$authLoginStub/auth-login-stub/gg-sign-in"
-
-  // Charities ---------------------------------
-
-  val CsrfPattern   = """<input type="hidden" name="csrfToken" value="([^"]+)""""
-  val lookupPattern = """<form method="POST" novalidate action="([^"]+)""""
-
-  def saveCsrfToken(): CheckBuilder[RegexCheckType, String] = regex(_ => CsrfPattern).saveAs("csrfToken")
-
-  private val lookupRegexp = "(.*)/begin"
-
-  def saveAddressLookupUrl: CheckBuilder[HttpHeaderRegexCheckType, Response] =
-    headerRegex("Location", lookupRegexp).saveAs("lookupAddressLocation")
 
 }
